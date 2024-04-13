8f976a04135a3745ec59eefc83cc46d810101599
salaudeen olanrewaju <olanrewajulanzy@gmail.com>
2024-04-13 21:27:06 +0100

    first commit

---
+++ sandbox.ts
@@ -0,0 +1,9 @@
+const instructor = "warees";
+console.log(instructor);
+
+const inputs = document.querySelectorAll("input");
+
+console.log(inputs);
+inputs.forEach((input) => {
+  console.log(input);
+});
